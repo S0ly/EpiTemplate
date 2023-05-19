4488db0ec3ece1e6147e9a0511da70c28fda2674
matis mounic <matis.mounic@epitech.eu>
2023-05-19 14:53:41 +0200

    ✨ added error handling to the basic_values

--- include/basic_values.h
+++ include/basic_values.h
@@ -8,17 +8,43 @@
 #ifndef BASIC_VALUES_H
     #define BASIC_VALUES_H
 
-    #define TRUE (1 == 1)
-    #define FALSE (!TRUE)
-    #ifndef NULL
+    #ifdef TRUE
+        #warning "TRUE has already been defined"
+    #else
+        #define TRUE (1 == 1)
+    #endif
+
+    #ifdef FALSE
+        #warning "FALSE has already been defined"
+    #else
+        #define FALSE (!TRUE)
+    #endif
+
+    #ifdef NULL
+        #warning "NULL has already been defined"
+    #else
         #define NULL ((void *)0)
     #endif
 
-    typedef char* string;
-    typedef int bool;
-    typedef struct Vector2f {
-        float x;
-        float y;
-    } Vector2f;
+    #ifdef string
+        #warning "string has already been defined"
+    #else
+        typedef char* string;
+    #endif
+
+    #ifdef bool
+        #warning "bool has already been defined"
+    #else
+        typedef int bool;
+    #endif
+
+    #ifdef Vector2f
+        #warning "Vector2f has already been defined"
+    #else
+        typedef struct Vector2f {
+            float x;
+            float y;
+        } Vector2f;
+    #endif
 
 #endif /* BASIC_VALUES_H */
